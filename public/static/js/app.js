dca99b3c976b764e53de71482b9552dff9181cc4
amitamit281 <amitamit281@rediffmail.com>
2017-03-28 22:08:57 -0700

    setup menus

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -6,21 +6,45 @@ zoombeiApp.config(function($routeProvider) {
 	$routeProvider.when('/index', {
 		templateUrl : 'views/main.html',
 		controller : 'mainCtrl'		
-	}).when('/vision', {
-		templateUrl : 'views/vision.html',
+	}).when('/home', {
+		templateUrl : 'views/main.html',
 		controller : 'mainCtrl'		
-	}).when('/mission', {
-	    templateUrl: 'views/mission.html',
+	}).when('/journey/vision', {
+		templateUrl : 'views/journey/vision.html',
+		controller : 'mainCtrl'		
+	}).when('/journey/mission', {
+	    templateUrl: 'views/journey/mission.html',
+	    controller: 'mainCtrl'
+	}).when('/solution/visual-analytic', {
+	    templateUrl: 'views/solution/visual-analytic.html',
+	    controller: 'mainCtrl'
+	}).when('/solution/employee-engagement', {
+	    templateUrl: 'views/solution/employee-engagement.html',
+	    controller: 'mainCtrl'
+	}).when('/solution/data-management', {
+	    templateUrl: 'views/solution/data-management.html',
+	    controller: 'mainCtrl'
+	}).when('/solution/research-service', {
+	    templateUrl: 'views/solution/research-service.html',
+	    controller: 'mainCtrl'
+	}).when('/solution/risk-management', {
+	    templateUrl: 'views/solution/risk-management.html',
 	    controller: 'mainCtrl'
 	}).when('/about', {
 	    templateUrl: 'views/about.html',
 	    controller: 'mainCtrl'
-	}).when('/home', {
-		templateUrl : 'views/main.html',
+	}).when('/people/leadership-team', {
+	    templateUrl: 'views/people/leadership-team.html',
+	    controller: 'mainCtrl'
+	}).when('/people/board-advisor', {
+	    templateUrl: 'views/people/board-advisor.html',
+	    controller: 'mainCtrl'
+	}).when('/people/management-team', {
+	    templateUrl: 'views/people/management-team.html',
+	    controller: 'mainCtrl'
+	}).when('/contact', {
+		templateUrl : 'views/contact.html',
 		controller : 'mainCtrl'		
-	}).when('/services', {
-		templateUrl : 'views/services.html',
-		controller : 'service-controller'		
 	}).when('/', {
 		templateUrl : 'views/main.html',
 		controller : 'mainCtrl'	
